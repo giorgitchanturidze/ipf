340b3f12ae0e20183e7c0879957183be338cf7c2
Giorgi Tchanturidze <chanturidzegiorgi31@gmail.com>
2024-07-09 21:46:11 +0400

    Adding blue header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+    theme: {
+        extend: {
+            screens: {
+                xl: "1440px",
+            },
+            colors: {
+                arctic: "#DDE5ED",
+                brightBlue: "#0074F0",
+                darkBlue: "#005EB8",
+                secondary: "#616161",
+                fire: "#FF3B00",
+                night: "#002677",
+                brightBlack: "#1B2F44",
+            },
+        },
+    },
+    plugins: [],
 }
